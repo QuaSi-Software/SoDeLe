7524fee5c73119a22f191ef5161cb5a12e81bf57
Jonas Mucke <jonas.mucke@web.de>
2023-09-20 17:14:18 +0200

    Updated the build as exe

--- buildAsExe.py
+++ buildAsExe.py
@@ -4,6 +4,7 @@ import PyInstaller.__main__
 PyInstaller.__main__.run([
     './bin/app.py',
     # add the /sodele folder as option for source code
-    '--add-data', './src/sodele;./src/sodele',
+    '-p', './src/',
+    '--add-data', './src/sodele/;./src/sodele/',
     '--onefile',
 ])
