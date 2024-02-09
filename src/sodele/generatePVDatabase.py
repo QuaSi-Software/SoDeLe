46a00f783c33a0a40a428b72cd42b837f4f54255
heiner.steinacker <heiner.steinacker@siz-energieplus.de>
2024-02-09 14:08:39 +0100

    bugfix and file renaming in generatePvDatabase

--- src/sodele/generatePVDatabase.py
+++ src/sodele/generatePVDatabase.py
@@ -16,15 +16,15 @@ def generatePvLibDatabase(basePath):
     :type basePath:     str
     :return:
     """
-    modules_path = f"{basePath}/221115_CEC_Modules.csv"
-    inverters_path = f"{basePath}/221115_CEC_Inverters.csv"
+    modules_path = f"{basePath}/CEC_Modules.csv"
+    inverters_path = f"{basePath}/CEC_Inverters.csv"
 
     if not os.path.exists(modules_path):
-        logging().warning(f"Fehler: Der Pfad zur PV-Moduldatenbank wurde nicht gefunden unter {basePath}")
+        logging().warning(f"Fehler: Der Pfad zur PV-Moduldatenbank wurde nicht gefunden unter {modules_path}. Der Dateiname muss CEC_Modules.csv sein.")
         assert False
 
     if not os.path.exists(inverters_path):
-        logging().warning(f"Fehler: Die PV-Moduldatenbank wurde nicht gefunden unter {modules_path}")
+        logging().warning(f"Fehler: Die Inverterdatenbank wurde nicht gefunden unter {inverters_path}. Der Dateiname muss CEC_Inverters.csv sein.")
         assert False
 
     PV_modules = pvlib.pvsystem.retrieve_sam(name=None, path=modules_path)
@@ -52,6 +52,8 @@ def generatePvLibDatabase(basePath):
     with open(writePath_Inverter, 'w') as f:
         OutputData_asString = PVInverter_names.to_string(header=False, index=False)
         f.write(OutputData_asString)
+
+    logging().info(f"The internal names of PV modules and inverters were written to {writePath_Modules} and {writePath_Inverter}.")
 
 
 if __name__ == "__main__":
